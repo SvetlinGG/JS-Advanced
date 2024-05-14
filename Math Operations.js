08b3792a6a9a56629c10ffa91439e2a2befa5f9d
Svetlin Garabedyan <146958421+SvetlinGG@users.noreply.github.com>
2024-05-14 15:31:50 +0200

    Add files via upload

---
+++ Math Operations.js
@@ -0,0 +1,28 @@
+function operations(num1, num2, operation){
+
+    let result = 0;
+    switch ( operation){
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+            case '*':
+                result = num1 * num2;
+                break;
+                case '/':
+            result = num1 / num2;
+            break;
+            case '**':
+            result = num1 ** num2;
+            break;
+            case '%':
+            result = num1 % num2;
+            break;
+    }
+    console.log(result);
+
+}
+operations(5, 6, '+');
+operations(3, 5.5, '*');
