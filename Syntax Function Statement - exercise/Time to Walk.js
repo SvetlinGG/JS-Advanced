6df0f69c518955ede19b26e4fe7134957443e9d1
Svetlin Garabedyan <146958421+SvetlinGG@users.noreply.github.com>
2024-05-15 21:36:31 +0200

    Add files via upload

---
+++ Syntax Function Statement - exercise/Time to Walk.js
@@ -0,0 +1,16 @@
+function walk(steps, footprint, speed) {
+    let distance = steps * footprint
+    let speedMetersSec = speed / 3.6
+    let time = distance / speedMetersSec
+    let rest = Math.floor(distance / 500)
+
+    let timeMin = Math.floor(time / 60)
+    let timeSec = Math.round(time - (timeMin * 60))
+    let timeHr = Math.floor(timeMin / 60)
+
+    timeMin -= timeHr * 60
+    timeMin += rest
+
+    console.log((timeHr < 10 ? "0" : "") + timeHr + ":" + (timeMin < 10 ? "0" : "") + timeMin + ":" + (timeSec < 10 ? "0" : "") + timeSec);
+}
+walk(4000, 0.60, 5)
