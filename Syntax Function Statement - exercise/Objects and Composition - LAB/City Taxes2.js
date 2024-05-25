976078ede05ef1c935c50541c32d1ea7e1b5c572
Svetlin Garabedyan <146958421+SvetlinGG@users.noreply.github.com>
2024-05-25 13:05:29 +0200

    Add files via upload

---
+++ Syntax Function Statement - exercise/Objects and Composition - LAB/City Taxes2.js
@@ -0,0 +1,34 @@
+function taxes(name, population, treasury){
+
+    let cityInfo = {
+        name: name,
+        population: Number(population),
+        treasury: Number(treasury),
+        taxRate: 10,
+
+        collectTaxes(){
+            this.treasury += this.population * this.taxRate;
+        },
+        applyGrowth(percentage){
+            this.population *= ( 100 + percentage) /100
+        },
+        applyRecession(percentage){
+            this.treasury *= ( 100 - percentage) /100
+        }
+
+
+
+    };
+    return cityInfo;
+    
+
+}
+
+const city = 
+  taxes('Tortuga',
+  7000,
+  15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
