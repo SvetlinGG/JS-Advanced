5b6ab06d1aef74f070c6037ea12d8b7fa37f8138
Svetlin Garabedyan <146958421+SvetlinGG@users.noreply.github.com>
2024-06-03 23:21:20 +0200

    Add files via upload

---
+++ Syntax Function Statement - exercise/Advanced Functions/List Processor.js
@@ -0,0 +1,20 @@
+function list(arr){
+
+    const result = {
+        value: [],
+        add(string){
+            this.values.push(string);
+        },
+        remove(string){
+            this.values =  this.values.filter(el => el !== string)
+        },
+        print(){
+            console.log(this.values.join(','));
+        },
+    };
+    arr.forEach(line => {
+       const [ command, value] = line.split(' ');
+       result[command](value); 
+    });
+}
+list(['add hello', 'add again', 'remove hello', 'add again', 'print']);
