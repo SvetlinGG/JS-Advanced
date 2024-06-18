4190715294ffd6f34da068e9ae532278024ecefb
Svetlin Garabedyan <146958421+SvetlinGG@users.noreply.github.com>
2024-06-18 03:02:11 +0200

    Update app.js

--- Syntax Function Statement - exercise/05.JS-Advanced-DOM-Manipulations-and-Events/05. Highlight-Active/app.js
+++ Syntax Function Statement - exercise/05.JS-Advanced-DOM-Manipulations-and-Events/05. Highlight-Active/app.js
@@ -1,3 +1,20 @@
 function focused() {
-    console.log('TODO:...');
+    
+
+    const inputSection = Array.from(document.querySelectorAll('input'));
+
+     for ( let el of inputSection){
+        el.addEventListener('focus', onFocus);
+        el.addEventListener('blur', onBlur);
+    };
+
+    function onFocus(e){
+        e.target.parentNode.classList.add('focused')
+
+    }
+
+    function onBlur(e){
+        e.target.parentNode.classList.remove('focused')
+
+    }
 }
